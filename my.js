b7ec53b6b8d4649dd90201fa6449fc7fa651357b
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2022-03-03 18:37:22 +0400

    added in personalinfo in inputs that alert deeds to hop when putting lest than two lettere

--- my.js
+++ my.js
@@ -1,3 +1,20 @@
 const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email')
+const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 
+
+
+fname.addEventListener('change', ()=> {
+    if(fname.value.length < 2) {
+        alert( fname.value + '    need more then two letters')
+    } 
+    
+})
+
+lname.addEventListener('change', ()=>{
+    if(lname.value.length < 2) {
+        alert(lname.value + ' need more then two letters')
+    }
+})
